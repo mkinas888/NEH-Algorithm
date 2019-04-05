c9d80e79c6b4e91d033040bd568c28380c8f65d1
mkinas888 <mkinas888@gmail.com>
2019-04-05 13:04:48 +0200

    Neh algorithm implemented on arrays

--- neh.cpp
+++ neh.cpp
@@ -7,20 +7,27 @@
 
 using namespace std;
 
-void swapTasksWithIds(int id1, int id2, int m, vector<vector<int>> &taskTable, vector<int> &idTable) {
+int taskTable[500][20];
+int currentTaskTable[500][20];
+int nextStepTable[500][20];
+int cTable[501][21];
+int idTable[500];
+int wTable[500];
+
+void swapTasksWithIds(int id1, int id2, int m) {
     for(int i=0;i<m;i++) {
-        swap(taskTable[id1][i], taskTable[id2][i]);
+        swap(currentTaskTable[id1][i], currentTaskTable[id2][i]);
     }
     swap(idTable[id1], idTable[id2]);
 }
 
-void swapTasks(int id1, int id2, int m, vector<vector<int>> &taskTable) {
+void swapTasks(int id1, int id2, int m) {
     for(int i=0;i<m;i++) {
-        swap(taskTable[id1][i], taskTable[id2][i]);
+        swap(nextStepTable[id1][i], nextStepTable[id2][i]);
     }
 }
 
-void countW(int n, int m, vector<vector<int>> taskTable, vector<int> &wTable) {
+void countW(int n, int m) {
     int currentSum;
     for(int i=0;i<n;i++) {
         currentSum = 0;
@@ -31,25 +38,22 @@ void countW(int n, int m, vector<vector<int>> taskTable, vector<int> &wTable) {
     }
 }
 
-int countCmax(int n, int m, vector<vector<int>> taskTable) {
+int countCmax(int n, int m, int tmpTable[500][20]) {
     int Cmax;
-    vector<vector<int>> cTable(n+1, vector<int> (m+1, 0));
     for(int i=0;i<n+1;i++) {
         for(int j=0;j<m+1;j++) {
             if(i == 0 || j == 0) {
                 cTable[i][j] = 0;
             } else {
-                cTable[i][j] = max(cTable[i-1][j], cTable[i][j-1]) + taskTable[i-1][j-1];
+                cTable[i][j] = max(cTable[i-1][j], cTable[i][j-1]) + tmpTable[i-1][j-1];
             }
         }
     }
     Cmax = cTable[n][m];
-    cTable.clear();
-    cTable.shrink_to_fit();
     return Cmax;
 }
 
-int getNextTaskId(int n, vector<int> &wTable) {
+int getNextTaskId(int n) {
     int id = 0, w = 0;
      for(int i=0;i<n;i++) {
         if(w < wTable[i]) {
@@ -61,15 +65,18 @@ int getNextTaskId(int n, vector<int> &wTable) {
     return id;
 }
 
-int performNextStep(int stepNumber, int m, vector<vector<int>> taskTable) {
+int performNextStep(int stepNumber, int m) {
+    for(int i = 0;i<stepNumber+1;i++) {
+        for(int j=0;j<m;j++) {
+            nextStepTable[i][j] = currentTaskTable[i][j];
+        }
+    }
     int Cmax = 0, tmp = 0, whichLoop = 0, loopNum = 0;
-    Cmax = countCmax(stepNumber+1, m, taskTable);
-    // cout << Cmax << endl;
+    Cmax = countCmax(stepNumber+1, m, nextStepTable);
     for(int i=stepNumber;i>0;i--) {
         loopNum++;
-        swapTasks(i, i-1, m, taskTable);
-        tmp = countCmax(stepNumber+1, m, taskTable);
-        // cout << tmp << endl;
+        swapTasks(i, i-1, m);
+        tmp = countCmax(stepNumber+1, m, nextStepTable);
         if(tmp <= Cmax) {
             Cmax = tmp;
             whichLoop = loopNum;
@@ -78,41 +85,25 @@ int performNextStep(int stepNumber, int m, vector<vector<int>> taskTable) {
     return whichLoop;
 }
 
-void performNeh(int n, int m, vector<vector<int>> taskTable, vector<int> &wTable, vector<int> &idTable) {
+void performNeh(int n, int m) {
     int nextId, loopId, tmp;
-    vector<vector<int>> currentTaskTable(n, vector<int> (m, 0));
-    nextId = getNextTaskId(n, wTable);
-    // cout << nextId + 1 << endl;
-    idTable.push_back(nextId);
+    nextId = getNextTaskId(n);
+    idTable[0] = nextId;
     for(int i=0;i<m;i++) {
         currentTaskTable[0][i] = taskTable[nextId][i];
     }
     for(int i=1;i<n;i++) {
-        nextId = getNextTaskId(n, wTable);
-        // cout << nextId + 1 << endl;
-        idTable.push_back(nextId);
+        nextId = getNextTaskId(n);
+        idTable[i] = nextId;
         for(int j=0;j<m;j++) {
             currentTaskTable[i][j] = taskTable[nextId][j];
         }
-        loopId = performNextStep(i, m, currentTaskTable);
+        loopId = performNextStep(i, m);
         tmp = i;
         for(int j=loopId;j>0;j--) {
-            swapTasksWithIds(tmp, tmp-1, m, currentTaskTable, idTable);
+            swapTasksWithIds(tmp, tmp-1, m);
             tmp--;
         }
-        // cout << endl;
-        // for(int j=0;j<idTable.size();j++) {
-        //     cout << idTable[j] + 1 << " ";
-        // }
-        // cout << endl;
-        // cout << countCmax(n, m, currentTaskTable) << endl;
-        //  for(int j=0;j<i+1;j++) {
-        //     for(int g=0;g<m;g++) {
-        //         cout << currentTaskTable[j][g] << " " << idTable[j] + 1;
-        //     }
-        //     cout << endl;
-        // }
-        // cout << loopId << endl;
     }
 
     cout << countCmax(n, m, currentTaskTable) << endl;
@@ -120,8 +111,6 @@ void performNeh(int n, int m, vector<vector<int>> taskTable, vector<int> &wTable
         cout << idTable[i] + 1 << " ";
     }
     cout << endl;
-    currentTaskTable.clear();
-    currentTaskTable.shrink_to_fit();
 }
 
 
@@ -149,32 +138,22 @@ int main () {
     }
     for(int i = 0; i<121;i++) {
         while(str != dataNames[i]) {
-        // while(str != "data.003:") {
+        // while(str != "data.002:") {
         data >> str;
         }
         data >> str;
         n = stoi(str);
         data >> str;
         m = stoi(str);
-        vector<vector<int>> taskTable(n, vector<int> (m, 0));
-        vector<int> idTable;
-        vector<int> wTable(n,0);
-        // load dataset to taskTable
         for(int y=0;y<n;y++) {
             for(int i=0;i<m;i++) {
                 data >> taskTable[y][i];
             }
         }
-        countW(n, m, taskTable, wTable);
-        performNeh(n, m, taskTable, wTable, idTable);
+        countW(n, m);
+        performNeh(n, m);
         cout << tmp << endl;
         tmp++;
-        taskTable.clear();
-        taskTable.shrink_to_fit();
-        idTable.clear();
-        idTable.shrink_to_fit();
-        wTable.clear();
-        wTable.shrink_to_fit();
     }
     auto end = chrono::steady_clock::now();
 	cout << "Elapsed time in seconds : "
